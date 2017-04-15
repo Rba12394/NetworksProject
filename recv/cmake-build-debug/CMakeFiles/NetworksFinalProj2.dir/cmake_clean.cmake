86b4531865d33639bfe7d6d7fd5604c806b86a66
vagrant <vagrant@cs281spring2017devbox>
2017-04-15 16:39:04 -0500

    commit from machine A

---
+++ recv/cmake-build-debug/CMakeFiles/NetworksFinalProj2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworksFinalProj2.dir/recvRawEth.c.o"
+  "NetworksFinalProj2.pdb"
+  "NetworksFinalProj2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NetworksFinalProj2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ recv/cmake-build-debug/CMakeFiles/NetworksFinalProj2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NetworksFinalProj2.
+# This may be replaced when dependencies are built.
