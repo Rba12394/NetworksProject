88d4086f1477910860eb94cbfa1f10575437cab0
vagrant <vagrant@cs281spring2017devbox>
2017-04-15 16:35:55 -0500

    proj from machine b

---
+++ cmake-build-debug/CMakeFiles/NetworksFinalProj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworksFinalProj.dir/sendRawEth.c.o"
+  "NetworksFinalProj.pdb"
+  "NetworksFinalProj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NetworksFinalProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
